2a5a14dede5b5d970af25fc01db04cdfe8e85b01
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-04-11 21:39:57 +0800

    Update channelchanger.py

--- channelchanger/channelchanger.py
+++ channelchanger/channelchanger.py
@@ -115,7 +115,7 @@ class ChannelChanger(commands.Cog):
                     newTitle = channelConfig.get("template").replace("X", channelConfig.get("name")).replace("Y", gameTitle)
                     
             if channel.name != newTitle:
-                await channel.edit(status=newTitle)
+                await channel.edit(name=newTitle)
 
     @commands.Cog.listener(name='on_voice_state_update')
     async def on_voice_state_update(self, member, before, after):
