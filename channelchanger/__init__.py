0d0404fb83cf5db8e978bf0b3a9f4ed585b9ff00
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-04-11 16:17:17 +0800

    Update __init__.py

--- channelchanger/__init__.py
+++ channelchanger/__init__.py
@@ -2,4 +2,4 @@ from .channelchanger import ChannelChanger
 from redbot.core.bot import Red
 
 async def setup(bot: Red) -> None:
-	await bot.add_cog(ChannelChanger())
+	await bot.add_cog(ChannelChanger(bot))
