3684c3376e2eecdd52a6f1ed68cb99fcb821e303
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2024-07-19 17:28:19 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -22,7 +22,25 @@ class Frappe(commands.Cog):
 
         await ctx.send(user.id)
         
+    @app_commands.command(name="id", description="Return the user ID")
+    @app_commands.describe(
+        user="user",
+    )
+    @app_commands.default_permissions()
+    @app_commands.guild_only()
+    async def slash_say(
+        self,
+        interaction: discord.Interaction,
+        user: Optional[str] = discord.Member=None,
+    ):
+        guild = interaction.guild
+        channel = channel or interaction.channel
 
+        author = interaction.author
+        if not user:
+            user = author
+        
+        await interaction.response.send_message(user.id, ephemeral=False)
 
     @commands.guild_only()
     @commands.is_owner()
