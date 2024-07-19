09e652dd1519c5a2673f33f74358c00c801d790d
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2024-07-19 18:17:09 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -39,6 +39,7 @@ class Frappe(commands.Cog):
         
         embed = discord.Embed()
         embed.set_footer(text="© Shadowzone Gaming")
+        embed.title = "Sponsorkliks"
         embed.colour = int("ff0502", 16)
         embed.add_field(name="Sponsorkliks", value=description, inline=False)
         embed.add_field(name="\u200B", value="-# P: In behandeling • A: Geaccepteerd • S: Ontvangen door Sponsorkliks • Q: Onderweg naar Shadowzone • T: Totaal overgemaakt", inline=False)
