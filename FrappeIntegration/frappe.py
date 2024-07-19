3f5aad7e966de47df4d045f266bd2f74972e0af0
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2024-07-19 15:16:58 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -14,7 +14,7 @@ class Frappe(commands.Cog):
     
     @tasks.loop(seconds=10)
     async def dailybirthday(self):
-        channel = 621338866955321345
+        channel = client.get_channel(621338866955321345)
         await channel.send("My task is running!")
         print("My task is running!")
 
