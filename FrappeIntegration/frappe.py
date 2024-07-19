bc72d0bdc10149fbd65e02bb7e0c1d9f0adba382
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2024-07-19 12:34:13 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -17,17 +17,18 @@ class Frappe(commands.Cog):
     @commands.hybrid_group()
     async def frappe(self, ctx: commands.Context) -> None:
         """Group of commands to use Frappe."""
+        pass
     
-        @frappe.command(aliases=["bd"])
-        @commands.has_permissions(manage_channels=True)
-        async def birthday(self, ctx: commands.Context):
-            frappe_keys = await self.bot.get_shared_api_tokens("frappe")
-            """Get birthdays of today"""
-            if frappe_keys.get("api_key") is None:
-                return await ctx.send("The Frappe API key has not been set. Use `[p]set api` to do this.")
-            api_key =  frappe_keys.get("api_key")
-            api_secret = frappe_keys.get("api_secret")
-            headers = {'Authorization': 'token' +api_key+ ':' +api_secret}
-            response = requests.get('http://shadowzone.nl/api/method/birthday', headers=headers)
+    @frappe.command(aliases=["bd"])
+    @commands.has_permissions(manage_channels=True)
+    async def birthday(self, ctx: commands.Context):
+        frappe_keys = await self.bot.get_shared_api_tokens("frappe")
+        """Get birthdays of today"""
+        if frappe_keys.get("api_key") is None:
+            return await ctx.send("The Frappe API key has not been set. Use `[p]set api` to do this.")
+        api_key =  frappe_keys.get("api_key")
+        api_secret = frappe_keys.get("api_secret")
+        headers = {'Authorization': 'token' +api_key+ ':' +api_secret}
+        response = requests.get('http://shadowzone.nl/api/method/birthday', headers=headers)
 
-            return await ctx.send(response.status_code)
+        return await ctx.send(response.status_code)
