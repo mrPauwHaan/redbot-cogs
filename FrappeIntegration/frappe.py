0f0622e214b3414e7d0da3bca60df9a2ae799b8e
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2024-07-24 15:09:39 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -231,5 +231,5 @@ class Frappe(commands.Cog):
             notfound = None
             amount_changes = 0
             if response['result']:
-                max = max(response['result'], key=lambda x:x['events'])
-                return await ctx.send(max)
+                maxevents = max(response['result'], key=lambda x:x['events'])
+                return await ctx.send(maxevents)
