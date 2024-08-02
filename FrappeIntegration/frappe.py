15a8126beb0536e1d0d665dc2c2ceb30baabb2b7
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2024-08-02 13:39:26 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -319,19 +319,20 @@ class Frappe(commands.Cog):
                                         if not serveruser:
                                             notfoundServertext = notfoundServertext + "<@" + str(user['discord_id']) + "> "
                                             notfoundServer.append(str(user['discord_id']))
-                                        elif not any(user['discord_id'] in x['member'] for x in data):
-                                            if user['events'] > 0:
-                                                userdata = {
-                                                    "events": user['events'],
-                                                    "member": user['discord_id'],
-                                                    "icon": "<:plus:1137646873042243625>",
-                                                }
-                                                await ctx.send(userdata)
-                                                data.append(userdata)
                             else:
                                 notfoundDatabase = notfoundDatabase + "<@" + str(member.id) + "> "
                     else:
                         await ctx.send("Rol voor `" + str(eventnumber) + " events` niet gevonden")
+                    
+                    for user in response['result']:
+                        if not any(user['discord_id'] in x['member'] for x in data):
+                            if user['events'] > 0:
+                                userdata = {
+                                    "events": user['events'],
+                                    "member": user['discord_id'],
+                                    "icon": "<:plus:1137646873042243625>",
+                                }
+                                data.append(userdata)
 
                 data.sort(key= lambda x:x['events'], reverse=True)
                 for data in data:
