91cb91d561caa0b3e3dcfef789d4ad3a8e6bef1f
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-09-14 18:40:03 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -109,12 +109,18 @@ class Frappe(commands.Cog):
         if api.status_code == 200:
             response = api.json()
             if response['data']:
-                banner = "http://shadowzone.nl/" + response['data'][0]['banner']
-                buffer = BytesIO(banner)
-                await ctx.guild.edit(
-                    banner=buffer,
-                    reason=f"ServerManage changing banner to {response['data'][0]['name']}",
-                )
+                banner_url = "http://shadowzone.nl/" + response['data'][0]['banner']
+                async with aiohttp.ClientSession() as session:
+                async with session.get(banner_url) as resp:
+                    if resp.status == 200:
+                        image_data = await resp.read()
+                        buffer = BytesIO(image_data)
+                        await ctx.guild.edit(
+                            banner=buffer,
+                            reason=f"ServerManage changing banner to {response['data'][0]['name']}",
+                        )
+                    else:
+                        await ctx.send("Failed to download the banner image.")
             else:
                 await ctx.send('No data found')
 
