cc9d127d41579ce469a04a3d206b618c061e3627
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-09-15 14:22:12 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -398,6 +398,7 @@ class Frappe(commands.Cog):
             return await ctx.send("Status code:" +str(api.status_code))
 
     @events.command()
+    @commands.has_permissions(administrator=True)
     async def aanmeldingen(self, ctx: commands.Context, event: str = None):
         """Krijg een lijst van de aanmeldingen voor een specifiek event"""
         deelnemers = self.Frappeclient.get_list('Event deelnemers', fields = ["event"], order_by = 'creation desc')
