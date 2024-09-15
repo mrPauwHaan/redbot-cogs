4f5b807024120567ff414dbbd939eaf4834753ae
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-09-15 09:04:03 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -135,10 +135,7 @@ class Frappe(commands.Cog):
                             api_secret = frappe_keys.get("api_secret")
                             doc = self.Frappeclient.get_doc('Discord server banners', response['data'][0]['name'])
                             doc['datum'] = '2018-01-01'
-                            try:
-                                self.Frappeclient.delete('Discord server banners', response['data'][0]['name'])
-                            except Exception as error:
-                                await ctx.send(error)
+                            doc.delete()
                             response2 = self.Frappeclient.insert(doc)
                             await ctx.send(doc)
                             await ctx.send(response2)
