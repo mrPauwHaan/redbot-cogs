65a44d7b3ee907bdc667015cef71b1b0fcb103fe
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-09-15 08:36:27 +0200

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -126,14 +126,17 @@ class Frappe(commands.Cog):
                     async with session.get(banner_url) as resp:
                         if resp.status == 200:
                             image_data = await resp.read()
-                            
+                            await ctx.guild.edit(
+                                banner=image_data,
+                                reason=f"De server banner is veranderd naar: {response['data'][0]['name']}",
+                            )
                             frappe_keys = await self.bot.get_shared_api_tokens("frappe")
                             api_key =  frappe_keys.get("api_key")
                             api_secret = frappe_keys.get("api_secret")
                             doc = self.Frappeclient.get_doc('Discord server banners', response['data'][0]['name'])
                             doc['datum'] = '2018-01-01'
-                            self.Frappeclient.update(doc)
-                            await ctx.send(doc)
+                            response = self.Frappeclient.update(doc)
+                            await ctx.send(response)
                                 
                         else:
                             await ctx.send("Failed to download the banner image")
