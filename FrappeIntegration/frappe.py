a82dbbd958096af37941e11fece5a0491231025a
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2025-01-08 12:26:12 +0100

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -130,7 +130,7 @@ class Frappe(commands.Cog):
     @commands.is_owner()
     async def contributie(self, ctx: commands.Context, jaar: int):
         """Check of contributie betaald is"""
-        data = self.Frappeclient.get_list('Member', fields = ['name','discord_id', 'custom_status'], filters = {}, order_by = 'member_name asc')
+        data = self.Frappeclient.get_list('Member', fields = ['name','discord_id', 'custom_status'], order_by = 'member_name asc', filters=None, limit_start=0, limit_page_length=0,)
         if data:
             message = ""
             for member in data:
