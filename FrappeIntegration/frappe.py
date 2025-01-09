18fee0d397e42067e798927fb10e74b4040e1048
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2025-01-09 11:58:38 +0100

    Update frappe.py

--- FrappeIntegration/frappe.py
+++ FrappeIntegration/frappe.py
@@ -144,7 +144,7 @@ class Frappe(commands.Cog):
                                     if resp.status == 200:
                                         image_data = await resp.read()
                                         with io.BytesIO(image_data) as file:
-                                            await channel.create_thread(name = aankondiging['titel'], content = aankondiging['text'] + '\n\n [Lees verder...](' + aankondiging['url'] + ') \n\n -# Of luister naar een door AI gegenereerde podcast over deze persoon:', file=discord.File(file, aankondiging['titel'] + ".wav"))
+                                            await channel.create_thread(name = aankondiging['titel'], content = aankondiging['text'] + '\n\n [Lees verder...](' + aankondiging['url'] + ') \n\n Of luister naar een door AI gegenereerde podcast over deze persoon:', file=discord.File(file, aankondiging['titel'] + ".wav"))
                     else:
                         await channel.create_thread(name = aankondiging['titel'], content = aankondiging['text'] + '\n\n [Lees verder...](' + aankondiging['url'] + ')')
 
