6406bb202e4db6f6e94f89647defda646928e210
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2025-04-16 19:29:12 +0200

    Update guildstats.py

--- guildstats/guildstats.py
+++ guildstats/guildstats.py
@@ -294,14 +294,14 @@ class GuildStats(Cog):
         # `created_on`
         draw.rounded_rectangle((1200, 75, 1545, 175), radius=15, fill=(47, 49, 54))
         align_text_center(
-            (1200, 75, 1545, 175),
+            (1200 - 1170, 75 + 779, 1545 - 1170, 175 + 779),
             text=_object.created_at.strftime("%d %B %Y"),
             fill=(255, 255, 255),
             font=self.font[36],
         )
-        draw.rounded_rectangle((1220, 30, 1476, 90), radius=15, fill=(79, 84, 92))
+        draw.rounded_rectangle((1220 - 1170, 30 + 779, 1476 - 1170, 90 + 779), radius=15, fill=(79, 84, 92))
         align_text_center(
-            (1220, 30, 1476, 90),
+            (1220 - 1170, 30 + 779, 1476 - 1170, 90 + 779),
             text="Op Discord",
             fill=(255, 255, 255),
             font=self.bold_font[30],
