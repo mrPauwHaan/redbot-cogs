13a34e963f159296636c6a700a4fdcd3ce023534
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2025-04-16 15:52:25 +0200

    Update guildstats.py

--- guildstats/guildstats.py
+++ guildstats/guildstats.py
@@ -446,7 +446,7 @@ class GuildStats(Cog):
                 draw.rounded_rectangle((1326, 712, 1892, 829), radius=15, fill=(32, 34, 37))
                 draw.rounded_rectangle((1326, 712, 1601, 829), radius=15, fill=(24, 26, 27))
                 align_text_center(
-                    (1326, 712, 1601, 829), text=events, fill=(255, 255, 255), font=self.bold_font[36]
+                    (1326, 712, 1601, 829), text=str(events), fill=(255, 255, 255), font=self.bold_font[36]
                 )
                 align_text_center(
                     (1601, 712, 1892, 829),
