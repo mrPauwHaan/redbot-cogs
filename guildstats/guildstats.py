46d833f083b0f1ee47eae956f9b549df218eae59
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2025-04-16 16:26:34 +0200

    Update guildstats.py

--- guildstats/guildstats.py
+++ guildstats/guildstats.py
@@ -432,8 +432,7 @@ class GuildStats(Cog):
                 # Events
                 events = 0
                 for item in member.get("custom_events"):
-                    raise Exception(item)
-                    if not item['event_bezocht'] == 'Qmusic Foute Party: 24 - 26 juni 2022' or 'Vakantie: 11-18 augustus 2023':
+                    if not item['event_bezocht'] == 'Qmusic Foute Party: 24 - 26 juni 2022':
                         events = events + 1
                 draw.rounded_rectangle((1306, 615, 1912, 996), radius=15, fill=(47, 49, 54))
                 align_text_center(
