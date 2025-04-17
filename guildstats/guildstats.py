22ceb92fc0f476235856f3a3590003865b3410c4
mrPauwHaan <jordy.pouwels@shadowzone.nl>
2025-04-17 10:49:03 +0200

    Update guildstats.py

--- guildstats/guildstats.py
+++ guildstats/guildstats.py
@@ -159,156 +159,145 @@ class GuildStats(Cog):
         )
         align_text_center = functools.partial(self.align_text_center, draw)
 
-        # Member name & Member avatar.
-        image = Image.open(io.BytesIO(_object_display))
-        image = image.resize((140, 140))
-        mask = Image.new("L", image.size, 0)
-        d = ImageDraw.Draw(mask)
-        d.rounded_rectangle(
-            (0, 0, image.width, image.height),
-            radius=20,
-            fill=255,
-        )
-        # d.ellipse((0, 0, image.width, image.height), fill=255)
-        try:
-            img.paste(
-                image, (30, 478, 170, 618), mask=ImageChops.multiply(mask, image.split()[3])
+        doc = self.Frappeclient.get_list('Member', fields = ['name'], filters = {'discord_id': _object.id})
+        if doc:
+            member = self.Frappeclient.get_doc("Member", doc[0]['name'])
+        
+            # Member name & Member avatar.
+            image = Image.open(io.BytesIO(_object_display))
+            image = image.resize((140, 140))
+            mask = Image.new("L", image.size, 0)
+            d = ImageDraw.Draw(mask)
+            d.rounded_rectangle(
+                (0, 0, image.width, image.height),
+                radius=20,
+                fill=255,
             )
-        except IndexError:
-            img.paste(image, (30, 478, 170, 618), mask=mask)
-        if (
-            sum(
-                (
-                    1
-                    if ord(char) in self.font_to_remove_unprintable_characters.getBestCmap()
-                    else 0
+            # d.ellipse((0, 0, image.width, image.height), fill=255)
+            try:
+                img.paste(
+                    image, (30, 478, 170, 618), mask=ImageChops.multiply(mask, image.split()[3])
                 )
-                for char in _object.display_name
-            )
-            / len(_object.display_name)
-            > 0.8
-        ) and len(self.remove_unprintable_characters(_object.display_name)) >= 5:
-            draw.text(
-                (190, 478),
-                text=self.remove_unprintable_characters(_object.display_name),
-                fill=(255, 255, 255),
-                font=self.bold_font[50],
-            )
-            display_name_size = self.bold_font[50].getbbox(_object.display_name)
+            except IndexError:
+                img.paste(image, (30, 478, 170, 618), mask=mask)
             if (
-                display_name_size[2]
-                + 25
-                + self.font[40].getbbox(_object.global_name or _object.name)[2]
-            ) <= 1000:
-                draw.text(
-                    (190 + display_name_size[2] + 25, 496),
-                    text=(
-                        self.remove_unprintable_characters(_object.global_name)
-                        if _object.global_name is not None
-                        else _object.name
-                    ),
-                    fill=(163, 163, 163),
-                    font=self.font[40],
-                )
-        elif (
-            _object.global_name is not None
-            and (
                 sum(
                     (
                         1
-                        if ord(char)
-                        in self.font_to_remove_unprintable_characters.getBestCmap()
+                        if ord(char) in self.font_to_remove_unprintable_characters.getBestCmap()
                         else 0
                     )
-                    for char in _object.global_name
+                    for char in _object.display_name
                 )
-                / len(_object.global_name)
+                / len(_object.display_name)
                 > 0.8
-            )
-            and len(self.remove_unprintable_characters(_object.global_name)) >= 5
-        ):
+            ) and len(self.remove_unprintable_characters(_object.display_name)) >= 5:
+                draw.text(
+                    (190, 478),
+                    text=self.remove_unprintable_characters(_object.display_name),
+                    fill=(255, 255, 255),
+                    font=self.bold_font[50],
+                )
+                display_name_size = self.bold_font[50].getbbox(_object.display_name)
+                if (
+                    display_name_size[2]
+                    + 25
+                    + self.font[40].getbbox(_object.global_name or _object.name)[2]
+                ) <= 1000:
+                    draw.text(
+                        (190 + display_name_size[2] + 25, 496),
+                        text=(
+                            self.remove_unprintable_characters(_object.global_name)
+                            if _object.global_name is not None
+                            else _object.name
+                        ),
+                        fill=(163, 163, 163),
+                        font=self.font[40],
+                    )
+            elif (
+                _object.global_name is not None
+                and (
+                    sum(
+                        (
+                            1
+                            if ord(char)
+                            in self.font_to_remove_unprintable_characters.getBestCmap()
+                            else 0
+                        )
+                        for char in _object.global_name
+                    )
+                    / len(_object.global_name)
+                    > 0.8
+                )
+                and len(self.remove_unprintable_characters(_object.global_name)) >= 5
+            ):
+                draw.text(
+                    (190, 478),
+                    text=self.remove_unprintable_characters(_object.global_name),
+                    fill=(255, 255, 255),
+                    font=self.bold_font[50],
+                )
+            else:
+                draw.text(
+                    (190, 478), text=_object.name, fill=(255, 255, 255), font=self.bold_font[50]
+                )
+
+            # Rol
             draw.text(
-                (190, 478),
-                text=self.remove_unprintable_characters(_object.global_name),
+                (265, 553),
+                text=f"{member.get('custom_titel_op_website') if member.get('custom_titel_op_website') else ''}",
+                fill=(163, 163, 163),
+                font=self.font[54],
+            )
+
+            # Guild name & Guild icon.
+            image = Image.open(self.icons["logo"])
+            image = image.resize((55, 55))
+            img.paste(image, (30, 30, 85, 85), mask=image.split()[3])
+            draw.text(
+                (105, 30),
+                text='Shadowzone Gaming',
+                fill=(163, 163, 163),
+                font=self.font[54],
+            )
+
+            # `created_on`
+            draw.rounded_rectangle((1200, 75, 1545, 175), radius=15, fill=(47, 49, 54))
+            align_text_center(
+                (1200, 75, 1545, 175),
+                text=_object.created_at.strftime("%d %B %Y"),
                 fill=(255, 255, 255),
-                font=self.bold_font[50],
+                font=self.font[36],
             )
-        else:
-            draw.text(
-                (190, 478), text=_object.name, fill=(255, 255, 255), font=self.bold_font[50]
+            draw.rounded_rectangle((1220, 30, 1476, 90), radius=15, fill=(79, 84, 92))
+            align_text_center(
+                (1220, 30, 1476, 90),
+                text="Op Discord",
+                fill=(255, 255, 255),
+                font=self.bold_font[30],
+            )
+            # `joined_on`
+            draw.rounded_rectangle((1200 + 365, 75, 1545 + 365, 175), radius=15, fill=(47, 49, 54))
+            align_text_center(
+                (1200 + 365, 75, 1545 + 365, 175),
+                text=_object.joined_at.strftime("%d %B %Y"),
+                fill=(255, 255, 255),
+                font=self.font[36],
+            )
+            draw.rounded_rectangle((1220 + 365, 30, 1476 + 365, 90), radius=15, fill=(79, 84, 92))
+            align_text_center(
+                (1220 + 365, 30, 1476 + 365, 90),
+                text="In server",
+                fill=(255, 255, 255),
+                font=self.bold_font[30],
             )
 
-        # Rol
-        image = Image.open(io.BytesIO(guild_icon))
-        image = image.resize((55, 55))
-        mask = Image.new("L", image.size, 0)
-        d = ImageDraw.Draw(mask)
-        d.rounded_rectangle(
-            (0, 0, image.width, image.height),
-            radius=25,
-            fill=255,
-        )
-        try:
-            img.paste(
-                image, (190, 553, 245, 608), mask=ImageChops.multiply(mask, image.split()[3])
-            )
-        except IndexError:
-            img.paste(image, (190, 553, 245, 608), mask=mask)
-        draw.text(
-            (265, 553),
-            text='Lid',
-            fill=(163, 163, 163),
-            font=self.font[54],
-        )
-
-        # Guild name & Guild icon.
-        image = Image.open(self.icons["logo"])
-        image = image.resize((55, 55))
-        img.paste(image, (30, 30, 85, 85), mask=image.split()[3])
-        draw.text(
-            (105, 30),
-            text='Shadowzone Gaming',
-            fill=(163, 163, 163),
-            font=self.font[54],
-        )
-
-        # `created_on`
-        draw.rounded_rectangle((1200, 75, 1545, 175), radius=15, fill=(47, 49, 54))
-        align_text_center(
-            (1200, 75, 1545, 175),
-            text=_object.created_at.strftime("%d %B %Y"),
-            fill=(255, 255, 255),
-            font=self.font[36],
-        )
-        draw.rounded_rectangle((1220, 30, 1476, 90), radius=15, fill=(79, 84, 92))
-        align_text_center(
-            (1220, 30, 1476, 90),
-            text="Op Discord",
-            fill=(255, 255, 255),
-            font=self.bold_font[30],
-        )
-        # `joined_on`
-        draw.rounded_rectangle((1200 + 365, 75, 1545 + 365, 175), radius=15, fill=(47, 49, 54))
-        align_text_center(
-            (1200 + 365, 75, 1545 + 365, 175),
-            text=_object.joined_at.strftime("%d %B %Y"),
-            fill=(255, 255, 255),
-            font=self.font[36],
-        )
-        draw.rounded_rectangle((1220 + 365, 30, 1476 + 365, 90), radius=15, fill=(79, 84, 92))
-        align_text_center(
-            (1220 + 365, 30, 1476 + 365, 90),
-            text="In server",
-            fill=(255, 255, 255),
-            font=self.bold_font[30],
-        )
-
-        if not to_file:
-            return img
-        buffer = io.BytesIO()
-        img.save(buffer, format="png", optimize=True)
-        buffer.seek(0)
-        return discord.File(buffer, filename="image.png")
+            if not to_file:
+                return img
+            buffer = io.BytesIO()
+            img.save(buffer, format="png", optimize=True)
+            buffer.seek(0)
+            return discord.File(buffer, filename="image.png")
 
     async def generate_prefix_image(
         self,
@@ -364,8 +353,7 @@ class GuildStats(Cog):
         # Data.
         if isinstance(_object, (discord.Member)):
             # lidmaatschap
-            userid = _object.id
-            doc = self.Frappeclient.get_list('Member', fields = ['name'], filters = {'discord_id': userid})
+            doc = self.Frappeclient.get_list('Member', fields = ['name'], filters = {'discord_id': _object.id})
             if doc:
                 member = self.Frappeclient.get_doc("Member", doc[0]['name'])
 
