fca82e5942a5c4f35768f0057edb5dcc723f12e7
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2026-08-22 13:17:50 +0200

    Update __init__.py

--- usercard/__init__.py
+++ usercard/__init__.py
@@ -1,31 +1,4 @@
-from redbot.core import errors  # isort:skip
-import importlib
-import sys
+from .usercard import UserCard
 
-try:
-    import AAA3A_utils
-except ModuleNotFoundError:
-    raise errors.CogLoadError(
-        "The needed utils to run the cog were not found."
-    )
-modules = sorted(
-    [module for module in sys.modules if module.split(".")[0] == "AAA3A_utils"], reverse=True
-)
-for module in modules:
-    try:
-        importlib.reload(sys.modules[module])
-    except ModuleNotFoundError:
-        pass
-del AAA3A_utils
-
-from redbot.core.bot import Red  # isort:skip
-from redbot.core.utils import get_end_user_data_statement
-
-from .usercard import usercard
-
-__red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
-
-
-async def setup(bot: Red) -> None:
-    cog = usercard(bot)
-    await bot.add_cog(cog)
+async def setup(bot):
+    await bot.add_cog(UserCard(bot))
